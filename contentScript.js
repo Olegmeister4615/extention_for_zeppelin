af29e6826371fdd07be4ec98b247a23cebe2e7b3
Olegmeister4615 <Olegmeister.9@yandex.ru>
2021-02-24 11:30:06 +0500

    yet another commit
    добавил фавиконок, выделил аудио в папку, сделал попап со ссылкой на гитхаб

--- contentScript.js
+++ contentScript.js
@@ -6,14 +6,14 @@ s.onload = function() {
 };
 var s2 = document.createElement('audio');
 // TODO: add "script.js" to web_accessible_resources in manifest.json
-s2.src = chrome.runtime.getURL('error.mp3');
+s2.src = chrome.runtime.getURL('audio_samples/error.mp3');
 s2.id="playerError";
 s2.onload = function() {
     this.remove();
 };
 var s3 = document.createElement('audio');
 // TODO: add "script.js" to web_accessible_resources in manifest.json
-s3.src = chrome.runtime.getURL('success.mp3');
+s3.src = chrome.runtime.getURL('audio_samples/success.mp3');
 s3.id="playerSuccess";
 s3.onload = function() {
     this.remove();
--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 let changeColor = document.getElementById('changeColor');
-
+/*
 chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
@@ -19,3 +19,17 @@ changeColor.onclick = function(element) {
         })
     });
 };
+*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.create({active: true, url: location});
+            };
+        })();
+    }
+});
